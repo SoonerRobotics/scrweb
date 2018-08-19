dc46e7e9fdd6aa5e93ea028f075bbf89d72fb32d
jkleiber <jkleiber8@gmail.com>
2018-08-18 19:58:47 -0500

    updates time of first meeting

--- upcoming_events.php
+++ upcoming_events.php
@@ -14,10 +14,10 @@ $events[0]['link']  = "";
 
 //Event 1
 $events[1]['title'] = "SCR First General Meeting";
-$events[1]['start'] = "TBA";
-$events[1]['end']   = "TBA";
+$events[1]['start'] = "Tuesday August 28th @ 7pm";
+$events[1]['end']   = "";
 $events[1]['city']  = "Norman, OK";
-$events[1]['venue'] = "TBA";
+$events[1]['venue'] = "Rawl Engineering Practice Facility";
 $events[1]['link']  = "";
 
 ////Event 2
