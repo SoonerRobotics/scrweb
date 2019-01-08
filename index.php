98c2a5a8c9d06815aba0730260a92d761f202aae
jkleiber <jkleiber8@gmail.com>
2019-01-08 16:28:23 -0600

    Adds social feeds to front page of the website

--- index.php
+++ index.php
@@ -2,7 +2,17 @@
 	<html lang="zxx" class="no-js">
 		<?php include "scr-header.php"; ?>
 		<body>	
-		  <?php include "navbar.php";?>
+            <!-- Include JavaScript SDK for Facebook feed integration -->
+            <div id="fb-root"></div>
+            <script>(function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2&appId=814259118915161&autoLogAppEvents=1';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>
+
+		    <?php include "navbar.php";?>
 
 			<!-- start banner Area -->
 			<section class="banner-area banner-area-home relative" id="home">
@@ -55,6 +65,55 @@
 			<?php
 				}
 			?>
+
+            <!-- Start social feeds -->
+            <section class="gallery-area section-qtr-gap">
+					<div class="container">
+						<div class="row d-flex justify-content-center">
+							<div class="menu-content pb-70 col-lg-8">
+								<div class="title text-center">
+									<h1 class="mb-10">Social</h1>
+								</div>
+							</div>
+						</div>
+
+                        
+                        <section class="social-feed-area">
+    	                    <div class="container">
+                                <div class="row d-flex justify-content-center">
+                                    <!-- Facebook -->
+                                    <div class="col-lg-4 social-feed-right">
+    		    	                    <!--<div class="social-feed-details">-->
+                                        <div class="fb-page" data-href="https://www.facebook.com/OURobots/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+                                            <blockquote cite="https://www.facebook.com/OURobots/" class="fb-xfbml-parse-ignore">
+                                                <a href="https://www.facebook.com/OURobots/">Sooner Competitive Robotics</a>
+                                            </blockquote>
+                                        </div>
+                                        <!--</div>-->
+                                    </div>
+                                    <!-- Instagram -->
+                                    <div class="col-lg-4 social-feed-right">
+    		    	                    <div class="social-feed-details">
+                                            <div id="pixlee_container"></div>
+                                            <script type="text/javascript">
+                                                window.PixleeAsyncInit = function() {Pixlee.init({apiKey:'ony2iod3Z1YFa9hS0b8A'});Pixlee.addSimpleWidget({widgetId:'13765'});};
+                                            </script>
+                                            <script src="//instafeed.assets.pixlee.com/assets/pixlee_widget_1_0_0.js"></script>
+                                        </div>
+                                    </div>
+                                    <!-- Twitter -->
+                                    <div class="col-lg-4 social-feed-right">
+    		    	                    <div class="social-feed-details">
+                                            <a class="twitter-timeline" href="https://twitter.com/OU_SCR?ref_src=twsrc%5Etfw">Tweets by OU_SCR</a> 
+                                            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>    
+                    </div>
+				</section>
+            <!-- End social feeds -->
 						
 			<!-- start footer Area -->		
 			<?php include "footer.php"; ?>
