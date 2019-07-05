bec36842b1f320f99b3effe10a60e9ea12edae2c
Noah Zemlin <noah.zemlin@gmail.com>
2019-07-05 13:10:29 -0500

    Make header not tiny and add github

--- social.php
+++ social.php
@@ -8,7 +8,8 @@ function footer_social()
     		<li><a href="https://www.facebook.com/OURobots/"><i class="fa fa-facebook"></i></a></li>
     		<li><a href="https://twitter.com/OU_SCR"><i class="fa fa-twitter"></i></a></li>
     		<li><a href="https://www.youtube.com/channel/UCBBtOvgnLrfVAYTP1tSAGrw?view_as=subscriber"><i class="fa fa-youtube"></i></a></li>
-    		<li><a href="https://www.instagram.com/ou_scr/"><i class="fa fa-instagram"></i></a></li>
+			<li><a href="https://www.instagram.com/ou_scr/"><i class="fa fa-instagram"></i></a></li>
+			<li><a href="https://github.com/soonerrobotics"><i class="fa fa-github"></i></a></li>
     	</ul>			
     </div>
 <?php
@@ -24,7 +25,8 @@ function header_social()
     		<li><a href="https://www.facebook.com/OURobots/"><i class="fa fa-facebook"></i></a></li>
     		<li><a href="https://twitter.com/OU_SCR"><i class="fa fa-twitter"></i></a></li>
     		<li><a href="https://www.youtube.com/channel/UCBBtOvgnLrfVAYTP1tSAGrw?view_as=subscriber"><i class="fa fa-youtube"></i></a></li>
-    		<li><a href="https://www.instagram.com/ou_scr/"><i class="fa fa-instagram"></i></a></li>
+			<li><a href="https://www.instagram.com/ou_scr/"><i class="fa fa-instagram"></i></a></li>
+			<li><a href="https://github.com/soonerrobotics"><i class="fa fa-github"></i></a></li>
     	</ul>			
     </div>
 <?php
@@ -42,6 +44,7 @@ function color_social()
 				<li><a href="https://twitter.com/OU_SCR"><i class="fa fa-twitter"></i></a></li>
 				<li><a href="https://www.youtube.com/channel/UCBBtOvgnLrfVAYTP1tSAGrw?view_as=subscriber"><i class="fa fa-youtube"></i></a></li>
 				<li><a href="https://www.instagram.com/ou_scr/"><i class="fa fa-instagram"></i></a></li>
+				<li><a href="https://github.com/soonerrobotics"><i class="fa fa-github"></i></a></li>
 			</ul>
 		</div>
 	</div>
