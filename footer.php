39f852d44a819c5f3105c5f3da2b49fd3d7563b7
Noah Zemlin <noah.zemlin@gmail.com>
2020-10-24 20:30:08 -0500

    Improve navbar and footer

--- footer.php
+++ footer.php
@@ -1,19 +1,20 @@
-<footer class="footer-area section-gap">
+<footer class="footer-area main-menu">
 	<div class="container">
-		<div class="row">																		
-			<div class="col-lg-4  col-md-6 col-sm-6">
-				<?php include "newsletter.php"; ?>
-			</div>											
-		</div>
 		<div class="footer-bottom row align-items-center justify-content-between">
-			<p class="footer-text m-0 col-lg-6 col-md-12">
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                Copyright &copy;
-                <script>document.write(new Date().getFullYear());</script> 
-                All rights reserved | This template is made with 
-                <i class="fa fa-heart-o" aria-hidden="true"></i> by 
-                <a href="https://colorlib.com" target="_blank">Colorlib</a>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+			<div class="m-0 col-lg-6 col-md-6">
+				<p>
+					<ul class="footer-social">
+						<li><a href="https://www.facebook.com/OURobots/"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="https://twitter.com/OU_SCR"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="https://www.youtube.com/channel/UCBBtOvgnLrfVAYTP1tSAGrw?view_as=subscriber"><i class="fa fa-youtube"></i></a></li>
+						<li><a href="https://www.instagram.com/ou_scr/"><i class="fa fa-instagram"></i></a></li>
+						<li><a href="https://github.com/soonerrobotics"><i class="fa fa-github"></i></a></li>
+					</ul>			
+				</p>
+				<p>
+					Copyright &copy; 2020 Sooner Competitive Robotics. All rights reserved.
+				</p>
+			</div>
 		</div>						
 	</div>
 </footer>	
